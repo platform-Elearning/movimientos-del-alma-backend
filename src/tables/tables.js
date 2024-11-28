f24fc2311f3e37ee0387d0cb8a60db079b52aed5
julimax <julimax951@gmail.com>
2024-11-28 16:59:24 -0300

    morgan + tables

---
+++ src/tables/tables.js
@@ -0,0 +1,39 @@
+import { pool } from "../db/configPG.js";
+
+export const createUsersTable = async () => {
+  const query = `
+    CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      username VARCHAR(50) NOT NULL,
+      password VARCHAR(100) NOT NULL,
+      role VARCHAR(20) NOT NULL
+    );
+  `;
+
+  try {
+    await pool.query(query);
+    console.log("Table 'users' created or already exists.");
+  } catch (error) {
+    console.error("Error creating table 'users':", error);
+  }
+};
+
+export const createStudentsTable = async () => {
+  const query = `
+    CREATE TABLE IF NOT EXISTS students (
+      id SERIAL PRIMARY KEY,
+      identificationNumber VARCHAR(50) NOT NULL,
+      name VARCHAR(50) NOT NULL,
+      lastname VARCHAR(50) NOT NULL,
+      nationality VARCHAR(50) NOT NULL,
+      email VARCHAR(255) UNIQUE NOT NULL
+    );
+  `;
+
+  try {
+    await pool.query(query);
+    console.log("Table 'students' created or already exists.");
+  } catch (error) {
+    console.error("Error creating table 'students':", error);
+  }
+};
