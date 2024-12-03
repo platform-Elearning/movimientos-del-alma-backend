daa3cc4feb57bf24df51fec7a6a70a22c30c2a6c
porlanagustin <porlan.agustin@gmail.com>
2024-12-02 22:25:07 -0300

    fix endpoints & initialize db with image in console

--- src/routes/routesCourses.js
+++ src/routes/routesCourses.js
@@ -6,12 +6,10 @@ const routerCourses = Router();
 // ROUTE FOR CREATE COURSE
 routerCourses.route("/createCourse").post(courseCreateController);
 
-
 // ROUTE FOR REGISTER TO COURSE
 routerCourses.route("/registerToCourse").post(enrollmentToCourseController);
 
 // ROUTER FOR GET ALL COURSES WITH STUDENT_ID
-
 routerCourses.route("/getCoursesById").get(getAllCoursesController);
 
 export default routerCourses;
